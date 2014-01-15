c91689fe97ee85772263b243dc69434ce5894093
Florent Cailhol <florent@oocube.com>
2014-01-15 22:59:52 +0100

    Add move command

--- bin/codefight.js
+++ bin/codefight.js
@@ -1,6 +1,9 @@
 function Game(players, map) {
   var hqLocations;
 
+  this.commands = [];
+  this.units = [];
+
   // Create the map
   this.map = new Map(map);
 
@@ -17,17 +20,24 @@ function Game(players, map) {
     var startingLocation = null;
 
     this._addUnit(player, startingLocation, UnitType.HQ);
-    this._addUnit(player, startingLocation, UnitType.BUILDER);
   }
 }
 
 Game.prototype = {
+  _addUnit: function(player, position, type) {
+    this.units.push(new Unit(player, type));
+  },
   update: function() {
     var i, n;
 
+    // Update units
+    for (i = 0, n = this.units.length; i < n; i++) {
+      this.units[i].update();
+    }
+
     // Process commands
     for (i = 0, n = this.commands.length; i < n; i++) {
-      this.commands[i].execute();
+      this.commands[i].call();
     }
 
     this.commands.length = 0;
